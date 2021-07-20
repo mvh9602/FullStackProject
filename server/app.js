a968278b022a56bebc6cc2a8ce5115aae7a50646
Matt Hoffman <mvh9602@g.rit.edu>
2021-07-19 21:49:11 -0400

    Await on mongoose connection promise

--- server/app.js
+++ server/app.js
@@ -21,13 +21,6 @@ const mongooseOptions = {
   useUnifiedTopology: true,
 };
 
-mongoose.connect(dbURL, mongooseOptions, (err) => {
-  if (err) {
-    console.log('Could not connect to database');
-    throw err;
-  }
-});
-
 let redisURL = {
   hostname: 'redis-13822.c276.us-east-1-2.ec2.cloud.redislabs.com',
   port: '13822',
@@ -54,42 +47,53 @@ console.log("Redis connection: " + redisClient.connected);
 
 const router = require('./router');
 
-const app = express();
-app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
-app.use(favicon(`${__dirname}/../hosted/img/favicon.ico`));
+(async () => {
+  try {
+    console.log(dbURL);
+    await mongoose.connect(dbURL, mongooseOptions);
+  
+    const app = express();
+    app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+    app.use(favicon(`${__dirname}/../hosted/img/favicon.ico`));
 
-app.use(compression());
-app.use(bodyParser.urlencoded({
-  entended: true,
-}));
-app.use(session({
-  key: 'sessionid',
-  store: new RedisStore({ client: redisClient }),
-  secret: 'Forum Fun',
-  resave: true,
-  saveUninitialized: true,
-  cookie: { httpOnly: true },
-}));
-app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-app.set('views', `${__dirname}/../views/`);
-app.use(cookieParser());
-app.disable('x-powered-by');
+    app.use(compression());
+    app.use(bodyParser.urlencoded({
+      entended: true,
+    }));
+    app.use(session({
+      key: 'sessionid',
+      store: new RedisStore({ client: redisClient }),
+      secret: 'Forum Fun',
+      resave: true,
+      saveUninitialized: true,
+      cookie: { httpOnly: true },
+    }));
+    app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
+    app.set('view engine', 'handlebars');
+    app.set('views', `${__dirname}/../views/`);
+    app.use(cookieParser());
+    app.disable('x-powered-by');
 
-app.use(csrf());
-app.use((err, req, res, next) => {
-  console.log(req);
-  if (err.code !== 'EBADCSRFTOKEN') return next(err);
+    app.use(csrf());
+    app.use((err, req, res, next) => {
+      console.log(req);
+      if (err.code !== 'EBADCSRFTOKEN') return next(err);
 
-  console.log('Missing CSRF token');
-  return false;
-});
+      console.log('Missing CSRF token');
+      return false;
+    });
 
-router(app);
+    router(app);
 
-app.listen(port, (err) => {
-  if (err) {
-    throw err;
+    app.listen(port, (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log(`Listening on port ${port}`);
+    });
+  } catch (err) {
+    console.error(err);
   }
-  console.log(`Listening on port ${port}`);
-});
+
+})();
+
